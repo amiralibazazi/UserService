62071cdaff9b880aa7902fe833e5fb91a7899cc8
Amir <amiralibazazi@gmail.com>
2014-09-11 15:51:15 +0100

    uservalidation now checks if the email and user name are valid and throws an exception for each otherwise

--- src/main/java/com/codurance/UserValidation.java
+++ src/main/java/com/codurance/UserValidation.java
@@ -2,6 +2,22 @@ package com.codurance;
 
 public class UserValidation {
     boolean returnValidityOf(User user) {
-        return (user.getFirstName() != "" && user.getLastName() != "");
+        return nameValidityOf(user) && emailValidityOf(user);
+    }
+
+    private boolean emailValidityOf(User user) {
+        if(user.getEmail().contains("@")) {
+            return true;
+        } else {
+            throw new RuntimeException("User email was not valid");
+        }
+    }
+
+    private boolean nameValidityOf(User user) {
+        if (user.getFirstName() != "" && user.getLastName() != "") {
+            return true;
+        } else {
+            throw new RuntimeException("User name was not valid");
+        }
     }
 }
--- src/test/java/com/codurance/UserValidationShould.java
+++ src/test/java/com/codurance/UserValidationShould.java
@@ -9,16 +9,28 @@ public class UserValidationShould {
 
 
     private static final User VALID_NEW_USER = new User(0, "sandro", "mancuso", "sandro@codurance.com");
+    private static final User USER_WITH_INVALID_EMAIL = new User(1, "amir", "bazazi", "amir.com"); //too many parameters?
+    private static final User USER_WITH_VALID_EMAIL = new User(1, "toby", "retallick", "toby@codurance.com"); //too many parameters?
     private static final User USER_WITH_INVALID_NAME = new User(1, "", "", "amir@codurance.com"); //too many parameters?
     private UserValidation userValidation = new UserValidation();
+
+    @Test(expected=RuntimeException.class) public void
+    throw_an_exception_if_user_name_is_invalid() {
+        userValidation.returnValidityOf(USER_WITH_INVALID_NAME);
+    }
 
     @Test public void
     check_if_a_user_has_a_valid_name() {
         assertThat(userValidation.returnValidityOf(VALID_NEW_USER), is(true));
     }
 
-    @Test public void
-    check_if_a_user_has_an_invalid_name() {
-        assertThat(userValidation.returnValidityOf(USER_WITH_INVALID_NAME), is(false));
+    @Test(expected = RuntimeException.class) public void
+    throw_an_exception_if_user_email_invalid() {
+        userValidation.returnValidityOf(USER_WITH_INVALID_EMAIL);
     }
+//    @Test public void
+//    check_if_a_user_has_a_valid_email() {
+//        assertThat(userValidation.returnValidityOf(USER_WITH_INVALID_EMAIL), is(false));
+//        assertThat(userValidation.returnValidityOf(USER_WITH_VALID_EMAIL), is(true));
+//    }
 }
